fc0de5de77689725c59266ed55dd02ade73df201
DamDamDev <damien.fasquelle@lapiscine.pro>
2024-12-06 15:05:01 +0100

    imagesymfony

--- backend/src/Controller/HomeController.php
+++ backend/src/Controller/HomeController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use App\Form\ImageType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +20,45 @@ class HomeController extends AbstractController
         // Passez la variable controller_name au template
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/upload', name: 'upload')]
+    public function upload(Request $request, EntityManagerInterface $em): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('filePath')->getData();
+
+            if ($file) {
+                $fileName = uniqid() . '.' . $file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Erreur lors de l\'upload.');
+                    return $this->redirectToRoute('upload');
+                }
+
+                $image->setFilePath($fileName);
+                $image->setUser($this->getUser());
+
+                $em->persist($image);
+                $em->flush();
+
+                $this->addFlash('success', 'Image uploadée avec succès.');
+                return $this->redirectToRoute('upload');
+            }
+        }
+
+        return $this->render('upload.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ backend/src/Entity/Image.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $filePath = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilePath(): ?string
+    {
+        return $this->filePath;
+    }
+
+    public function setFilePath(string $filePath): static
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
